0bb352a57192199b473aab7de571128b5396e1be
Kyamel <129009324+Kyamel@users.noreply.github.com>
2024-08-11 21:45:18 -0300

    refactor: agora o log da busca binaria mostra mais casas decimais

--- src/utils/log.c
+++ src/utils/log.c
@@ -23,6 +23,6 @@ void log_total_time(FILE* log_file, clock_t start_time) {
     if (log_file) {
         clock_t end_time = clock();
         double total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-        fprintf(log_file, "Tempo total de busca binaria: %.2f segundos\n", total_time);
+        fprintf(log_file, "Tempo total de busca binaria: %f segundos\n", total_time);
     }
 }
